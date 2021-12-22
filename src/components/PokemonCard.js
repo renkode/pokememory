f76f838981e08566ebd91cbe7ff3b0424e8cc196
Renko <ren.tran@protonmail.com>
2021-12-21 18:34:10 -0800

    scaffold

---
+++ src/components/PokemonCard.js
@@ -0,0 +1,14 @@
+import React, { Component, useState } from "react";
+import styled from "styled-components";
+
+const PokemonCard = (props) => {
+  return <div />;
+};
+
+// shuffleCards()
+// difficulty (num of cards)
+// include/exclude regions in settings
+// reset button
+// pick pokemon from array of generations
+
+export default PokemonCard;
